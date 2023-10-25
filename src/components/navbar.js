f80d92735bd512785658ec4a54a469aa7db4e61c
naleesther <esthernalenyi@gmail.com>
2023-10-25 10:12:02 +0300

    nav bar responsiveness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,51 @@
 import './navbar.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars,faTimes } from "@fortawesome/free-solid-svg-icons";
+
 import Logo from "./img/logo.png";
 import Home from "./img/Home.png";
+import { useState } from 'react';
 
 function Navbar() {
+const [open,setOpen]=useState(false);
+const openNav =()=>{
+  setOpen(!open)
+}
   return (
     <div>
-      <nav>
+      <nav className={`navbar ${open ? "active" : ""}`}>
         <div className="navTop">
           <img className="logo" src={Logo} />
-          <div className="elements">
-            <ul>
-              <li>
+          <div>
+            <div className="menu-icon" onClick={openNav}>
+              {open ? (
+                <FontAwesomeIcon icon={faTimes} />
+              ) : (
+                <FontAwesomeIcon icon={faBars} />
+              )}
+            </div>
+            <ul className={`nav-element ${open ? "active" : ""}`}>
+              <li className="nav-items">
                 <a className="home">HOME</a>
               </li>
-              <li>
+              <li className="nav-items">
                 <a>ABOUT</a>
               </li>
-              <li>
+              <li className="nav-items">
                 <a>PORTFOLIO</a>
               </li>
-              <li>
+              <li className="nav-items">
                 <a>SERVICES</a>
               </li>
-              <li>
+              <li className="nav-items">
                 <a>CONTACTS</a>
               </li>
             </ul>
-
-            <FontAwesomeIcon icon={faBars} className="menu" />
           </div>
         </div>
       </nav>
       <div className="container">
-        <img className="HOME" src={Home} />
+        {/* <img className="HOME" src={Home} /> */}
         <div className="texts">
           <div className="vision">Your Vision, Our Expertise</div>
           <div className="dream">BUILDING YOUR DREAM HOME</div>
